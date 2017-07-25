f0b33e63be546851dcfd4443901e0932a41ae049
Abdusa Alam <abdusa.alam@tokopedia.com>
2017-07-25 16:15:27 +0700

    Initial commit

---
+++ config/browserSync.js
@@ -0,0 +1,17 @@
+module.exports = {
+    "ui": false,
+    "files": [
+        'public/index.html',
+        'public/assets/css/style.css',
+        'public/assets/img/**/*.{png, jpg, jpeg, svg}',
+        'public/assets/fonts/**/*.{otf, eot, ttf, svg, woff, woff2}',
+        'public/assets/js/*.js'
+    ],
+    "server": {
+        baseDir: 'public',
+        index: 'index.html',
+    },
+    "port": 4000,
+    "notify": false,
+    "injectChanges": true
+};
